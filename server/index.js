f7744e10251b4edca098c2eba26a46b4b3824e76
Atharvashuks <atharvashuklaas@gmail.com>
2022-04-26 13:34:45 +0530

    final version

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import postRoutes from './routes/posts.js';
-import userRoutes from './routes/user.js';
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/user.js";
 
 const app = express();
 dotenv.config();
@@ -14,12 +14,16 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts' , postRoutes);
-app.use('/user', userRoutes);
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello  to Blogries API");
+});
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL)
-.then(() => app.listen(PORT, () => console.log(`Server active on ${PORT}`)))
-.catch((err) => console.error(err));
-
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server active on ${PORT}`)))
+  .catch((err) => console.error(err));
